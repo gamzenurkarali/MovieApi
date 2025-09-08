d2db095c4162dc7a45fd2976c2ff9f5f4a53ac1d
gamze-tech <info.gmztech@gmail.com>
2025-09-08 14:43:57 +0300

    Katmanların oluşturulması, entitylerin eklenmesi, migrations, CQRS başlangıç

---
+++ Infrastructure/MovieApi.Persistence/Context/MovieContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MovieApi.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Internal;
+namespace MovieApi.Persistence.Context
+{
+    public class MovieContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-SMOAMK0\\SQLEXPRESS;initial catalog=ApiMovieDB;integrated security=true;TrustServerCertificate=true");
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Cast> Casts { get; set; }
+
+
+    }
+}
